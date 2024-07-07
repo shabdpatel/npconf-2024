4d49afc124ac4d079463de9923cff7cc0a8cb6db
shabd patel <shabdpatel0@gmail.com>
2024-07-07 19:25:37 +0530

    cokkies

--- src/components/socialwall/Socialwall.jsx
+++ src/components/socialwall/Socialwall.jsx
@@ -23,16 +23,16 @@ const Socialwall = () => {
             <h2 className="text-4xl md:text-5xl font-bold text-white mb-5 md:mb-5 text-center">Our Social Wall</h2>
             <p className="text-center text-gray-300 text-2xl mb-8">Get updated on our latest events and workshops</p>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <div className="bg-white shadow-md rounded-lg p-4">
-                    {/* Twitter section */}
+                {/* <div className="bg-white shadow-md rounded-lg p-4">
+                   
                     {twitterError ? (
                         <p className="text-center text-red-500">Failed to load Twitter content.</p>
                     ) : (
                         <TwitterTweetEmbed tweetId="1806178714134176000" />
                     )}
-                </div>
-                <div className="bg-white shadow-md rounded-lg p-4">
-                    {/* LinkedIn section */}
+                </div> */}
+                {/* <div className="bg-white shadow-md rounded-lg p-4">
+                   
                     <iframe
                         src="https://www.linkedin.com/embed/feed/update/urn:li:share:7211943110178684928"
                         height="1047"
@@ -41,7 +41,7 @@ const Socialwall = () => {
                         allowFullScreen=""
                         title="Embedded LinkedIn post">
                     </iframe>
-                </div>
+                </div> */}
             </div>
         </div>
     );
