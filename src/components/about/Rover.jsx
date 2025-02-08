a569e0d33606bf4e730e5a2a4f1dcb2ae97fc740
Sivichoudhary <shivangibeniwal338@gmail.com>
2025-02-08 19:35:07 +0530

    Rover update

--- src/components/about/Rover.jsx
+++ src/components/about/Rover.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "./Loader";
 
 const RoverModel = ({ isMobile }) => {
-  const rover = useGLTF("./Perseverance.gltf");
+  const rover = useGLTF("/Perseverance.gltf");
 
   useEffect(() => {
     rover.scene.traverse((child) => {
